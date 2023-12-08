9e1ba798a33da72b603ea8be43381045ec2d8bed
“Nithin <ravurinithinchowdary@gmail.com>
2023-12-08 10:10:24 -0600

    Added file 108

---
+++ leet_code_108/main.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        if len(nums)==0:
+            return None
+        if len(nums)==1:
+            return TreeNode(nums[0])
+        mid = len(nums)//2
+        left = self.sortedArrayToBST(nums[:mid])
+        right = self.sortedArrayToBST(nums[mid+1:])
+        return TreeNode(nums[mid], left, right)   
