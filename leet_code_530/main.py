940ccb47437302f210fdd390f154770cdf8bd085
“Nithin <ravurinithinchowdary@gmail.com>
2024-01-10 15:28:05 -0600

    Added 530 file

---
+++ leet_code_530/main.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def getMinimumDifference(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        arr = []
+        def preorder(root):
+            if not root:
+                return
+            
+            arr.append(root.val)
+            preorder(root.left)
+            preorder(root.right)
+        preorder(root)
+        arr = sorted(arr)
+        res = float('inf')
+
+        for i in range(1, len(arr)):
+            res = min(res, abs(arr[i])-abs(arr[i-1]))
+        return res
+     
