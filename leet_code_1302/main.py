72ed1f099bf87adbb95f0653e7e152820955a338
“Nithin <ravurinithinchowdary@gmail.com>
2024-01-11 09:40:32 -0600

    Added 1302 file

---
+++ leet_code_1302/main.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def deepestLeavesSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        queue = deque([root])
+        while queue:
+            n = len(queue)
+            temp = []
+            for _ in range(n):
+                node = queue.popleft()
+                temp.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return sum(temp)
+        
