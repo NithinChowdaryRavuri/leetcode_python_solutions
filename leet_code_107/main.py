33259119db52b87d66aef7f654c6ba01fcbbb3d1
“Nithin <ravurinithinchowdary@gmail.com>
2024-01-16 09:40:48 -0600

    Added 107 file

---
+++ leet_code_107/main.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return
+        queue = deque([root])
+        res = []
+        while queue:
+            n = len(queue)
+            temp = []
+            for _ in range(n):
+                node = queue.popleft()
+                temp.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(temp)
+        return res[::-1]
