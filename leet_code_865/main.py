20faf1629ddbb9dcff07a2563d9765b860d5a0d6
“Nithin <ravurinithinchowdary@gmail.com>
2024-01-22 19:43:07 -0600

    Added 865 file

---
+++ leet_code_865/main.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def subtreeWithAllDeepest(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        def dfs(root):
+            if not root: return None, 0
+            left, ld = dfs(root.left)
+            right, rd = dfs(root.right)
+            if ld==rd:
+                return root, ld+1
+            elif ld>rd:
+                return left, ld+1
+            else:
+                return right, rd+1
+        result, _ = dfs(root)
+        return result
+        
