7c2607b2705c036fae99370bdf63237b450452f0
“Nithin <ravurinithinchowdary@gmail.com>
2024-03-08 21:03:51 -0600

    Added 2540 file

---
+++ leet_code_2540/main.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def getCommon(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        n1, n2 = len(nums1), len(nums2)
+        i, j = 0, 0
+        while i<n1 and j<n2:
+            if nums1[i]==nums2[j]:
+                return nums1[i]
+            if nums1[i]<nums2[j]:
+                i+=1
+            else:
+                j+=1
+        return -1
