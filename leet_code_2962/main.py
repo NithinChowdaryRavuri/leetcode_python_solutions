7ed8888218ffa41897e025b1313709c7a09dc34f
“Nithin <ravurinithinchowdary@gmail.com>
2024-03-29 12:09:54 -0500

    Added 2962 file

---
+++ leet_code_2962/main.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def countSubarrays(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        mx = max(nums)
+        ans = cnt_mx = left = 0
+        for x in nums:
+            if x == mx:
+                cnt_mx += 1
+            while cnt_mx == k:
+                if nums[left] == mx:
+                    cnt_mx -= 1
+                left += 1
+            ans += left
+        return ans
