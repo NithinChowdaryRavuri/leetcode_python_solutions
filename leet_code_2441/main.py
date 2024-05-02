ed23cd9aa30896701f273f5de93f57700c019c75
Nithin_Chowdary_Ravuri <ravurinithinchowdary@gmail.com>
2024-05-02 09:44:42 -0500

    Added 2441 file

---
+++ leet_code_2441/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMaxK(self, nums: List[int]) -> int:
+        nums.sort()
+        i, j = 0, len(nums)-1
+        while i<j:
+            if -1*nums[i] == nums[j]:
+                return nums[j]
+            if -1*nums[i] > nums[j]:
+                i +=1
+            else:
+                j -= 1
+        return -1
